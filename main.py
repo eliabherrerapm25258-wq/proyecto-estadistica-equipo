7429f6c89b7836327802fd8b10512bc888ff22aa
Brayan Johan <eliab.herrera_pm25258@cecyte.edu.mx>
2026-04-15 10:36:55 -0600

    Estructura inical del proyecto

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    print("--- Analizador de datos v1.0---")
+    datos = [10,20.30,40,50,60] # datos de prueba
+    print(f"Datos a procesar: {datos}")
+
+if __name__ == "__main__":
+    main()
