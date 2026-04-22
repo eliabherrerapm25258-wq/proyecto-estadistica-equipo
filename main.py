953bec4e6d0b0ef5ea23d24aeca843e713479755
Benjamin Uc Reyes <miguel.uc_pm25248@cecyte.edu.mx>
2026-04-22 10:30:27 -0600

    se agregaron cambios en la rama main

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ def multiplicacion (a,b):
     rest = a * b
     return rest
 
+""""
+Realiza la multiplicacion de dos numeros:
+
+argumentos:
+   a (int):primer numero a multiplicar
+   b (int):segundo numero a multiplicar
+
+retorno:
+    (int): resultado de la multiplicacion de dos numeros
+"""
 def main():
     print("--- Analizador de datos v1.0---")
     datos = [10,20.30,40,50,60] # datos de prueba
     print(f"Datos a procesar: {datos}")
 
 if __name__ == "__main__":
-    main()
+    main() 
